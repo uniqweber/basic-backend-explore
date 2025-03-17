6c0490b409f0298721ac5ed771fb58894c7b043b
uniqweber <uniqwebersocial@gmail.com>
2025-03-17 22:28:54 +0600

    fs basic exploration done

--- 02-fs-basic/fileSystem.js
+++ 02-fs-basic/fileSystem.js
@@ -1,37 +1,37 @@
 const fs = require("fs");
 
-// fs.writeFile("hello.txt", "hello world", (err) => {
-//   console.log(err ? err : "file created");
-// });
+fs.writeFile("hello.txt", "hello world", (err) => {
+  console.log(err ? err : "file created");
+});
 
-// fs.appendFile("propose.txt", "hello", () => {
-//   console.log(err ? err : "new text added");
-// });
+fs.readFile("hello.txt", (err, files) => {
+  console.log(err ? err : files.toString());
+});
 
-// fs.rename("propose2.txt", "propose3.txt", (err) => {
-//   console.log(err ? err : "file renamed");
-// });
+fs.appendFile("propose.txt", "hello", () => {
+  console.log(err ? err : "new text added");
+});
 
-// fs.copyFile("propose4.txt", "propose4.txt", (err) => {
-//   console.log(err ? err : "file copied");
-// });
+fs.unlink("propose3.txt", (err) => {
+  console.log(err ? err : "file deleted");
+});
 
-// fs.unlink("propose3.txt", (err) => {
-//   console.log(err ? err : "file deleted");
-// });
+fs.rename("propose2.txt", "propose3.txt", (err) => {
+  console.log(err ? err : "file renamed");
+});
 
-// fs.readFile("hello.txt", (err, files) => {
-//   console.log(err ? err : files.toString());
-// });
+fs.copyFile("propose4.txt", "propose4.txt", (err) => {
+  console.log(err ? err : "file copied");
+});
 
-// fs.mkdir("new/something/hello.txt", {recursive: true}, (err) => {
-//   console.log(err ? err : "folder created");
-// });
-
-// fs.rm("something/new", {recursive: true}, (err) => {
-//   console.log(err ? err : "folder deleted");
-// });
+fs.mkdir("new/something/hello.txt", {recursive: true}, (err) => {
+  console.log(err ? err : "folder created");
+});
 
 fs.readdir("./", (err, files) => {
   console.log(err ? err : files);
+});
+
+fs.rm("something/new", {recursive: true}, (err) => {
+  console.log(err ? err : "folder deleted");
 });
