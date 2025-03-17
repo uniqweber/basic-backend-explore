01906876f638d290f254904e3232036a29457b2a
uniqweber <uniqwebersocial@gmail.com>
2025-03-17 19:01:45 +0600

    basic port creation explored

--- 01-node-basic/nodeBasic.js
+++ 01-node-basic/nodeBasic.js
@@ -1,1 +1,9 @@
+var http = require("http");
 
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end("<h1>hello</h1>");
+  })
+  .listen(3000);
+console.log("Server running at http://localhost:3000/");
